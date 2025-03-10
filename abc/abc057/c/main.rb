35fc98509d68c9bb43c547adcf6d0cfd8053acf9
reckyy <srreki98@gmail.com>
2025-03-10 10:47:33 +0900

    abc057 c AC

--- abc/abc057/c/main.rb
+++ abc/abc057/c/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split(" ")
+n = gets.to_i
+n_sqrt = Math.sqrt(n).floor
+ans = n
+
+(1..n_sqrt).each do |i|
+  if n % i == 0
+    a_digits = i.digits.length
+    b_digits = (n / i).digits.length
+
+    max = [a_digits, b_digits].max
+    ans = [ans, max].min
+  end
+end
+
+puts ans
