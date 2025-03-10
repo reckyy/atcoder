16f678ba60cc678064223f701da810815643155f
reckyy <srreki98@gmail.com>
2025-03-10 10:32:38 +0900

    abc120 b修正

--- abc/abc120/b/main.rb
+++ abc/abc120/b/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 a, b, k = gets.chomp.split.map(&:to_i)
 count = 0
-max = [a, b].max
+max = [a, b].min # [a, b].maxとしていたが、minでよかった。
 
 max.downto(1) do |i|
   count += 1 if ((a % i) == 0) && ((b % i) == 0)
