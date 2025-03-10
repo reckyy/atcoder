3cfb20148bcf0142674ea18e02bb9b603ffa9d0a
reckyy <srreki98@gmail.com>
2025-03-10 10:31:37 +0900

    abc120 bのみ

---
+++ abc/abc120/b/main.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+a, b, k = gets.chomp.split.map(&:to_i)
+count = 0
+max = [a, b].max
+
+max.downto(1) do |i|
+  count += 1 if ((a % i) == 0) && ((b % i) == 0)
+
+  if count == k
+    puts i
+    exit
+  end
+end
