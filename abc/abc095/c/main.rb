d626ba21a5de05c506dcb1de2fc3a9688c79b209
reckyy <srreki98@gmail.com>
2025-03-10 11:35:53 +0900

    abc095 c AC

--- abc/abc095/c/main.rb
+++ abc/abc095/c/main.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split(" ")
+a, b, c, x, y = gets.chomp.split.map(&:to_i)
+
+a_price = a * x
+b_price = b * y
+ab_pattern_price = [x, y].max * c * 2
+
+if x == y
+  puts [(a_price + b_price), ab_pattern_price].min
+else
+  shortage_pizza_price = x > y ? a : b
+  ab_and_shortage_price = [x, y].min * c * 2 + ((x - y).abs * shortage_pizza_price)
+
+  puts [ab_pattern_price, ab_and_shortage_price, (a_price + b_price)].min
+end
