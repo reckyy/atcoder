407e187726930aa07a3f395227fa2fecdf049763
reckyy <srreki98@gmail.com>
2025-03-10 13:02:00 +0900

    abc150 c AC

--- abc/abc150/c/main.rb
+++ abc/abc150/c/main.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split(" ")
+n = gets.to_i
+p = gets.chomp.split.map(&:to_i)
+q = gets.chomp.split.map(&:to_i)
+permutation = (1..n).to_a.permutation
+
+p_index = permutation.find_index(p)
+q_index = permutation.find_index(q)
+
+puts (p_index - q_index).abs
