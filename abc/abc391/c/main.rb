3e6e02d990fbf56460d3f0f440b94c704fb7aac3
reckyy <srreki98@gmail.com>
2025-03-11 21:30:57 +0900

    abc391 c AC

--- abc/abc391/c/main.rb
+++ abc/abc391/c/main.rb
@@ -1,2 +1,26 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split(" ")
+n, q = gets.split.map(&:to_i)
+pigeon_nest = Array.new(n, 1)
+pigeon_place = (1..n).map { |index| [index, index] }.to_h
+ans = 0
+
+q.times do
+  query = gets.split.map(&:to_i)
+
+  if query[0] == 1
+    p = query[1]
+    h = query[2]
+    from = pigeon_place[p]
+    to = h
+
+    ans -= 1 if pigeon_nest[from - 1] == 2
+    pigeon_nest[from - 1] -= 1
+
+    pigeon_nest[to - 1] += 1
+    ans += 1 if pigeon_nest[to - 1] == 2
+    
+    pigeon_place[p] = to
+  else
+    puts ans
+  end
+end
