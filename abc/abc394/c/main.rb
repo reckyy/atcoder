17d842a028f7217954a6d50bc869c2b1672018da
reckyy <srreki98@gmail.com>
2025-03-11 16:50:08 +0900

    abc394 c 解説AC

--- abc/abc394/c/main.rb
+++ abc/abc394/c/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split(" ")
+# refer to answer
+s = gets.chomp.chars
+i = 0
+
+while(i < s.length - 1)
+  if (s[i] == 'W') && (s[i + 1] == 'A')
+    s[i] = 'A'
+    s[i + 1] = 'C'
+    i -= 1 if i > 0
+  else
+    i += 1
+  end
+end
+
+puts s.join
