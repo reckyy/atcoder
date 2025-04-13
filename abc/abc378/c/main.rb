02e856c44ee5bb755a58ac96ff7e596ff4291c12
reckyy <srreki98@gmail.com>
2025-04-13 22:14:02 +0900

    abc378 c AC

--- abc/abc378/c/main.rb
+++ abc/abc378/c/main.rb
@@ -1,3 +1,10 @@
 #!/usr/bin/env ruby
 n = gets.to_i
 a = gets.split.map(&:to_i)
+hash = {}
+ans = []
+a.each.with_index(1) do |num, i|
+  ans << (hash[num] ? hash[num] : -1)
+  hash[num] = i
+end
+puts ans.join(' ')
