a06cdb55abc81b8e213727ea4fd9fda0f420fdce
reckyy <srreki98@gmail.com>
2025-04-13 22:26:50 +0900

    abc348 c AC

--- abc/abc348/c/main.rb
+++ abc/abc348/c/main.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split(" ")
+n = gets.to_i
+color_and_value = Hash.new(Float::INFINITY)
+n.times do
+  a, c = gets.split.map(&:to_i)
+  color_and_value[c] = [color_and_value[c], a].min
+end
+puts color_and_value.values.max
