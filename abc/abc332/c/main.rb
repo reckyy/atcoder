2cd7739d154f82a9ebb1fd39f57992d682fa8914
reckyy <srreki98@gmail.com>
2025-04-14 11:41:59 +0900

    abc332 c AC

--- abc/abc332/c/main.rb
+++ abc/abc332/c/main.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split(" ")
+n, m = gets.split.map(&:to_i)
+s = gets.split('').map(&:to_i)
+shirt = m
+logo_shirt = 0
+needed_logo = 0
+s.each do |schedule|
+  case schedule
+  when 0 then shirt, logo_shirt = m, needed_logo
+  when 1 then shirt == 0 ? (logo_shirt == 0 ? needed_logo += 1 : logo_shirt -= 1) : shirt -= 1
+  when 2 then logo_shirt == 0 ? needed_logo += 1 : logo_shirt -= 1
+  end
+end
+puts needed_logo
