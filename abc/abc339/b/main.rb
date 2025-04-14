9e39e341ec216c1a14ea928acc8af3544e6abe9d
reckyy <srreki98@gmail.com>
2025-04-14 10:57:22 +0900

    abc339 b AC

--- abc/abc339/b/main.rb
+++ abc/abc339/b/main.rb
@@ -1,1 +1,22 @@
 #!/usr/bin/env ruby
+h, w, n = gets.split.map(&:to_i)
+grid = Array.new(h) { ('.' * w).chars }
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1]
+idx = 0
+i = j = 0
+n.times do
+  if grid[i][j] == '.'
+    grid[i][j] = '#'
+    idx = (idx + 1) % 4
+  else
+    grid[i][j] = '.'
+    idx = (idx + 3) % 4
+  end
+
+  i = (i + di[idx]) % h
+  j = (j + dj[idx]) % w
+end
+grid.each do |g|
+  puts g.join
+end
